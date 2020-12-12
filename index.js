904f85e4080d28be8d33de77a3b856b61ee47fdd
nshelly <dannyjos@yandex.ru>
2020-12-12 11:56:55 +0300

    Fix error with osmID

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Nominatim = require('nominatim-geocoder')
 const fs = require('fs')
 const axios = require('axios');
 const geocoder = new Nominatim()
+const { StringDecoder } = require('string_decoder');
+const translator = require('cyrillic-to-translit-js')
 
 fs.readFile('src/cities.txt', (err, data) => {
     if (err) throw err;
     const dataString = Buffer.from(data)
     let cities = dataString.toString().split('\n')
-    for (let i = 0; i < cities.length; i++) {
+    for (let i = 0; i < cities.length - 1; i++) {
         findOSMID(cities[i])
     }
 });
@@ -34,7 +36,9 @@ function openPolygonPage(id, city)
             getJsonLimits(id)
         })
         .catch(function (error) {
-            console.log('Error reading http://polygons.openstreetmap.fr/?id=' + id + '\n Bad OSM ID, trying again...');
+            console.log('Error reading http://polygons.openstreetmap.fr/?id=' + id + '\n Bad OSM ID, trying other way...');
+            otherWayGetJson(id, city)
+
         })
         .then(function () {
         });
@@ -56,4 +60,20 @@ function getJsonLimits(id){
         });
 }
 
-//269701
+function otherWayGetJson(id, city){
+    city = translator().transform(city, '');
+    let url = 'https://nominatim.openstreetmap.org/search?city='+city+'&polygon_geojson=1&format=json'
+    axios.get(url)
+        .then(function (response) {
+            let file_data = JSON.stringify(response.data[1].geojson)
+            let path = 'Cities/CityLimits_' + id + '.json'
+            console.log('saving file... ' + 'CityLimits_' + id + '.json');
+            fs.writeFileSync( path, file_data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+        });
+}
+
